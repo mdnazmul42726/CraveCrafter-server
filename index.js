60ea74250af361db49da6687b7f201a569edff79
mdnazmul42726 <nazmulofficial@outlook.com>
2023-11-05 23:11:55 +0600

    All the top selling foods are extracted from the database and sent to the client site

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4ip7zr9.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true, } });
+
+async function run() {
+    try {
+
+        await client.connect();
+
+        const topFoodCollection = client.db("craveDB").collection("topSellingFoods")
+
+        app.get('/foods/top/v1', async (req, res) => {
+            const cursor = await topFoodCollection.find().toArray()
+            res.send(cursor)
+        });
+
+        app.get('/top-food/v1/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await topFoodCollection.findOne(query);
+            res.send(result)
+        });
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', async (req, res) => res.send('CraveCrafter Server is Running'))
 app.listen(port, () => console.log('CraveCrafter Server is Running On PORT:', port));
