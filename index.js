ac0e9484f0ff6b6c18fbec5b1fed680c913f57f8
mdnazmul42726 <nazmulofficial@outlook.com>
2023-11-07 04:03:57 +0600

    The food ordered by the user is fetched from the database and sent to the client side

--- index.js
+++ index.js
@@ -97,6 +97,13 @@ async function run() {
 
         });
 
+        app.delete('/orders/delete/v1/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.send(result);
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
