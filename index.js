7557f337cd664a1b25a2b9f8e6de31c221405082
mdnazmul42726 <nazmulofficial@outlook.com>
2023-11-06 15:19:54 +0600

    The details of the food ordered by the client are received at the server and stored in the database

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4ip7zr9.mongodb.net/?retryWrites=true&w=majority`;
@@ -20,6 +20,7 @@ async function run() {
 
         const topFoodCollection = client.db("craveDB").collection("topSellingFoods")
         const foodsCollection = client.db("craveDB").collection("foods")
+        const ordersCollection = client.db("craveDB").collection("orders")
 
         app.get('/foods/top/v1', async (req, res) => {
             const cursor = await topFoodCollection.find().toArray()
@@ -42,6 +43,19 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await foodsCollection.findOne(query);
+            res.send(result)
+        });
+
+        app.get('/top-food/v1/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await topFoodCollection.findOne(query);
+            res.send(result)
+        });
+
+        app.post('/food/order/v1', async (req, res) => {
+            const orderData = req.body;
+            const result = await ordersCollection.insertOne(orderData);
             res.send(result)
         });
 
